6a9d5bd467c044c14987b6a2b295735be0d23921
lucaslingle <lucasdaxlingle@gmail.com>
2021-12-31 02:42:27 -0800

    commit updates

--- drl/envs/wrappers/episodic_life.py
+++ drl/envs/wrappers/episodic_life.py
@@ -2,21 +2,24 @@ from drl.envs.wrappers.abstract import Wrapper
 
 
 ATARI_LIFES_FN = lambda env: env.unwrapped.ale.lives()
+ATARI_NOOP_ACTION = 0
 
 
 class EpisodicLifeWrapper(Wrapper):
     """
     Use loss-of-life to mark end-of-episode using the done indicator.
     """
-    def __init__(self, env, lives_fn):
+    def __init__(self, env, lives_fn, noop_action):
         """
         :param env (gym.core.Env): OpenAI gym environment instance.
         :param lives_fn (Callable[[Env], int]): Function to obtain num lives.
+        :param noop_action (int): Any no-op action.
         """
         super().__init__(self, env)
         self._was_real_done = True
         self._lives = 0
         self._lives_fn = lives_fn
+        self._noop_action = noop_action
 
     def step(self, action):
         obs, reward, self._was_real_done, info = self.env.step(action)
@@ -33,6 +36,6 @@ class EpisodicLifeWrapper(Wrapper):
         if self._was_real_done:
             obs = self.env.reset(**kwargs)
         else:
-            obs, *_ = self.env.step(0)
-        self._lives = self.env.unwrapped.ale.lives()
+            obs, *_ = self.env.step(self._noop_action)
+        self._lives = self._lives_fn(self.env)
         return obs
