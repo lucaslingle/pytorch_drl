711c73cd646586248425fe828ad4dc745ebd101f
lucaslingle <lucasdaxlingle@gmail.com>
2022-01-01 18:38:57 -0800

    commit updates

--- drl/envs/wrappers/stateful/random_network_distillation.py
+++ drl/envs/wrappers/stateful/random_network_distillation.py
@@ -1,9 +1,6 @@
-from typing import Dict, Any
-
 import torch as tc
 from torch.nn.parallel import DistributedDataParallel as DDP
 
-from drl.utils.typing_util import Env
 from drl.envs.wrappers.common.abstract import Wrapper
 from drl.envs.wrappers.stateful.abstract import TrainableWrapper
 from drl.envs.wrappers.stateful.normalize import Normalizer
@@ -12,17 +9,33 @@ from drl.algos.metrics import global_mean
 from drl.utils.optim_util import get_optimizer
 
 
+class RNDNetwork(tc.nn.Module):
+    def __init__(self, data_shape, widening):
+        super().__init__()
+        self._widening = widening
+        self._preprocessing = ToChannelMajor()
+        self._network = tc.nn.Sequential()
+
+    def _init_weights(self):
+        # todo(lucaslingle): use the good init from OpenAI repo here.
+        raise NotImplementedError
+
+    def forward(self):
+        raise NotImplementedError
+
+
 class TeacherNetwork(tc.nn.Module):
     """
     Teacher network for Random Network Distillation.
     Assumes input image has dimension 84x84x4.
     """
-    def __init__(self, data_shape, width_multiplier=1):
+    def __init__(self, data_shape, widening=1):
         super().__init__()
         self._data_shape = data_shape
         self._network = tc.nn.Sequential(
-            ToChannelMajor(),
+            RNDNetwork(data_shape, widening),
         )
+        # todo(lucaslingle): add flat, maybe act, and proj
 
     def forward(self, x):
         raise NotImplementedError
@@ -33,37 +46,45 @@ class StudentNetwork(tc.nn.Module):
     Student network for Random Network Distillation.
     Assumes input image has dimension 84x84x4.
     """
-    def __init__(self, data_shape, width_multiplier=1):
+    def __init__(self, data_shape, widening=1):
         super().__init__()
-        self._data_shape = data_shape
         self._network = tc.nn.Sequential(
-            ToChannelMajor(),
+            RNDNetwork(data_shape, widening),
         )
+        # todo(lucaslingle):
+        #    add flat, maybe act, and fc layers as per github impl from OpenAI.
 
     def forward(self, x):
         raise NotImplementedError
 
 
 class RandomNetworkDistillationWrapper(TrainableWrapper):
-    def __init__(
-            self,
-            env: Env,
-            rnd_optimizer_cls_name: str,
-            rnd_optimizer_args: Dict[str, Any],
-            world_size: int,
-            width_multipler: int = 1
-    ):
+    """
+    Random Network Distillation wrapper. See Burda et al., 2018 for details.
+
+    This class supports distributed data parallel training,
+    and synchronizes normalization statistics across processes.
+    """
+    def __init__(self, env, rnd_opt_cls_name, rnd_opt_args, world_size, widening):
+        """
+        Args:
+            env (Env): OpenAI gym environment instance.
+            rnd_opt_cls_name (str): Optimizer class name.
+            rnd_opt_args (Dict[str, Any]): Optimizer args.
+            world_size (int): Number of processes.
+            widening (int): Channel multiplier for networks.
+        """
         super().__init__(env)
         self._data_shape = (84, 84, 4)
         self._world_size = world_size
         self._synced_normalizer = Normalizer(self._data_shape, -5, 5)
         self._unsynced_normalizer = Normalizer(self._data_shape, -5, -5)
-        self._teacher_net = DDP(TeacherNetwork(self._data_shape, width_multipler))
-        self._student_net = DDP(StudentNetwork(self._data_shape, width_multipler))
+        self._teacher_net = DDP(TeacherNetwork(self._data_shape, widening))
+        self._student_net = DDP(StudentNetwork(self._data_shape, widening))
         self._optimizer = get_optimizer(
             model=self._student_net,
-            optimizer_cls_name=rnd_optimizer_cls_name,
-            optimizer_args=rnd_optimizer_args)
+            optimizer_cls_name=rnd_opt_cls_name,
+            optimizer_args=rnd_opt_args)
         self._run_checks()
         self._sync_normalizers()
 
