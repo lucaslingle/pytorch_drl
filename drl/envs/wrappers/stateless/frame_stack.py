73df2c5117d37ce090457eab15cf76efbc554bf3
lucaslingle <lucasdaxlingle@gmail.com>
2022-01-08 18:09:45 -0800

    commit updates

--- drl/envs/wrappers/stateless/frame_stack.py
+++ drl/envs/wrappers/stateless/frame_stack.py
@@ -7,37 +7,37 @@ from drl.envs.wrappers.stateless.abstract import Wrapper
 
 
 class FrameStackWrapper(Wrapper):
-    def __init__(self, env, num_stack):
+    def __init__(self, env, num_frames):
         """
         Args:
             env (Env): OpenAI gym environment instance.
-            num_stack (int): Number of frames to stack.
+            num_frames (int): Number of frames to stack.
         """
         super().__init__(env)
-        self._num_stack = num_stack
+        self._num_frames = num_frames
         self._frames = deque(maxlen=self._num_frames)
         self._set_observation_space()
 
     def _set_observation_space(self):
         start_shape = self.env.observation_space.shape
-        stacked_shape = (*start_shape[:-1], start_shape[-1] * self._num_stack)
+        stacked_shape = (*start_shape[:-1], start_shape[-1] * self._num_frames)
         dtype = self.env.observation_space.dtype
         self.observation_space = gym.spaces.Box(
             low=0, high=255, shape=stacked_shape, dtype=dtype)
 
     def _get_obs(self):
-        assert len(self.frames) == self._num_stack
-        return LazyFrames(list(self.frames))
+        assert len(self._frames) == self._num_frames
+        return LazyFrames(list(self._frames))
 
     def reset(self, **kwargs):
         obs = self.env.reset()
-        for _ in range(self.k):
-            self.frames.append(obs)
+        for _ in range(self._num_frames):
+            self._frames.append(obs)
         return self._get_obs()
 
     def step(self, action):
         ob, reward, done, info = self.env.step(action)
-        self.frames.append(ob)
+        self._frames.append(ob)
         return self._get_obs(), reward, done, info
 
 
