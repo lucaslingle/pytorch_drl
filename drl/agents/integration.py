c5685e9d38f5a17f57e0ff843420600e68e0f713
lucaslingle <lucasdaxlingle@gmail.com>
2022-01-22 03:26:48 -0800

    commit updates

--- drl/agents/integration.py
+++ drl/agents/integration.py
@@ -5,7 +5,7 @@ import torch as tc
 import gym
 
 from drl.agents.preprocessing import Preprocessing
-from drl.agents.architectures import Architecture
+from drl.agents.architectures.stateless.abstract import StatelessArchitecture
 from drl.agents.heads import (
     Head,
     DiscreteActionValueHead,
@@ -42,7 +42,6 @@ def get_predictor(cls_name, cls_args):
 
 def get_predictors(env, **predictors_spec: Dict[str, Dict[str, Any]]):
     predictors = dict()
-
     for key, spec in predictors_spec.items():
         # infer number of actions or action dimensionality.
         if key == 'policy' or key.startswith('action_value'):
@@ -53,17 +52,14 @@ def get_predictors(env, **predictors_spec: Dict[str, Dict[str, Any]]):
             else:
                 msg = "Unknown action space."
                 raise TypeError(msg)
-
         # create and add the predictor.
         predictor = get_predictor(**spec)
         predictors[key] = predictor
-
         # add additional predictor for epsilon-greedy policy in DQN.
         if isinstance(predictor, DiscreteActionValueHead):
             eps_greedy_policy_predictor = EpsilonGreedyCategoricalPolicyHead(
                 action_value_head=predictor)
             predictors['policy'] = eps_greedy_policy_predictor
-
     return predictors
 
 
@@ -71,15 +67,36 @@ class Agent(tc.nn.Module):
     def __init__(
             self,
             preprocessing: List[Preprocessing],
-            architecture: Architecture,
+            architecture: StatelessArchitecture,
             predictors: Dict[str, Head],
             detach_input: bool = True
     ):
+        """
+        Args:
+            preprocessing: List of preprocessing operations.
+            architecture: StatelessArchitecture instance.
+            predictors: Dictionary of prediction heads, keyed by prediction name.
+            detach_input: Detach input or not? Default: True.
+                This is a no-op if used with external observation tensors,
+                since these are outside the model.parameters() passed to the
+                optimizer and moreover default to have requires_grad=False.
+        """
         super().__init__()
         self._preprocessing = tc.nn.Sequential(*preprocessing)
         self._architecture = architecture
         self._predictors = tc.nn.ModuleDict(predictors)
         self._detach_input = detach_input
+        self._check_predict_keys()
+
+    def _check_predict_keys(self):
+        for key in self.keys:
+            if key == 'policy':
+                continue
+            if key.startswith('value_'):
+                continue
+            if key.startswith('action_value_'):
+                continue
+            raise ValueError(f"Prediction key {key} not supported.")
 
     @property
     def keys(self):
