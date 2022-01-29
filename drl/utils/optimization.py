401e0a7c9b2a787410dc38d9dc558c542ede6821
lucaslingle <lucasdaxlingle@gmail.com>
2022-01-29 03:15:31 -0800

    commit updates

--- drl/utils/optimization.py
+++ drl/utils/optimization.py
@@ -1,7 +1,34 @@
+from typing import Dict, Any, Optional, List, Union
 import importlib
 
+import torch as tc
 
-def get_optimizer(model, cls_name, cls_args):
+from drl.utils.types import Module, Optimizer, Scheduler
+
+
+def get_optimizer(
+        model: Module,
+        cls_name: str,
+        cls_args: Dict[str, Any]
+) -> Optimizer:
+    """
+    Creates an optimizer.
+
+    Args:
+        model (Module): Torch module instance.
+        cls_name (str): Optimizer class name. Must correspond to a derived class
+            of torch.optim.Optimizer.
+        cls_args (Dict[str, Any]): Dictionary of arguments passed to the
+            class constructor.
+
+    Returns:
+        torch.optim.Optimizer: Instantiated optimizer.
+
+    Note:
+        If the cls_name is 'AdamW', our implementation only applies weight decay
+        to the weights, and not biases/normalization parameters. To disable this,
+        you can use cls_name 'Adam' instead.
+    """
     module = importlib.import_module('torch.optim')
     cls = getattr(module, cls_name)
     if cls_name == 'AdamW':
@@ -11,7 +38,25 @@ def get_optimizer(model, cls_name, cls_args):
     return cls(model.parameters(), **cls_args)
 
 
-def get_scheduler(optimizer, cls_name, cls_args):
+def get_scheduler(
+        optimizer: Optimizer,
+        cls_name: str,
+        cls_args: Dict[str, Any]
+) -> Optional[Scheduler]:
+    """
+    Creates a learning rate scheduler.
+
+    Args:
+        optimizer (torch.optim.Optimizer): Torch optimizer to wrap.
+        cls_name (str): Scheduler class name, which should correspond
+            to a derived class of torch.optim.lr_scheduler._LRScheduler,
+            or 'None'.
+        cls_args: Dictionary of arguments passed to class constructor.
+
+    Returns:
+        Optional[torch.optim.lr_scheduler._LRScheduler]: Instantiated scheduler
+            or None.
+    """
     if cls_name == 'None':
         return None
     module = importlib.import_module('torch.optim.lr_scheduler')
@@ -19,7 +64,23 @@ def get_scheduler(optimizer, cls_name, cls_args):
     return cls(optimizer, **cls_args)
 
 
-def get_weight_decay_param_groups(model, wd):
+def get_weight_decay_param_groups(
+        model: Module,
+        wd: float
+) -> List[Dict[str, Union[tc.nn.parameter.Parameter, float]]]:
+    """
+    Splits a model's parameters into two parameter groups:
+        those to apply weight decay to, and those not to.
+
+    Args:
+        model (torch.nn.Module): Torch module instance.
+        wd (float): Weight decay coefficient.
+
+    Returns:
+        List[Dict[str, Union[tc.nn.parameter.Parameter, float]],
+           Dict[str, Union[tc.nn.parameter.Parameter, float]]]:
+           List of two dictionaries with keys 'params', and 'weight_decay'.
+    """
     apply_decay, no_decay = [], []
     for name, param in model.named_parameters():
         if not param.requires_grad:
