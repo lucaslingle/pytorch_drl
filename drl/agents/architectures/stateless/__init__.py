18b64260ea42a7e89e44532a256b641672d783dc
lucaslingle <lucasdaxlingle@gmail.com>
2022-01-31 03:46:24 -0800

    commit updates

--- drl/agents/architectures/stateless/__init__.py
+++ drl/agents/architectures/stateless/__init__.py
@@ -1,7 +1,11 @@
+from drl.agents.architectures.stateless.abstract import (
+    StatelessArchitecture, HeadEligibleArchitecture
+)
 from drl.agents.architectures.stateless.async_cnn import AsyncCNN
 from drl.agents.architectures.stateless.dueling import DuelingArchitecture
 from drl.agents.architectures.stateless.linear import Linear
 from drl.agents.architectures.stateless.mlp import MLP
+from drl.agents.architectures.stateless.nature_cnn import NatureCNN
 
 __all__ = [
     "StatelessArchitecture",
--- drl/algos/__init__.py
+++ drl/algos/__init__.py
@@ -1,4 +1,5 @@
 from drl.algos.abstract import Algo
+from drl.algos.ppo import PPO
 
 __all__ = [
     "Algo",
