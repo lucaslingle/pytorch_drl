c8e556092e1a141069d2cb00c9e9f2bc1b283f5b
lucaslingle <lucasdaxlingle@gmail.com>
2022-02-11 05:13:30 -0800

    commit updates

--- drl/algos/ppo.py
+++ drl/algos/ppo.py
@@ -209,11 +209,11 @@ class PPO(Algo):
             predictions = self._policy_net(
                 trajectory['observations'], predict=policy_predict)
             pi = predictions.pop('policy')
-            if self._value_net:
+            if not self._value_net:
+                vpreds = predictions
+            else:
                 vpreds = self._value_net(
                     trajectory['observations'], predict=value_predict)
-            else:
-                vpreds = predictions
 
             # shallow copy of trajectory dict, point to initial/new values.
             trajectory_new = {
