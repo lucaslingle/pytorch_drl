bbe2a2ea72c95a28165ad640a9c20b4c06fece8a
lucaslingle <lucasdaxlingle@gmail.com>
2022-02-15 19:01:52 -0800

    commit updates

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ setup(
     description="Pytorch library implementing Deep RL algorithms.",
     author="Lucas D. Lingle",
     install_requires=[
-        'torch==1.10.1',
-        'tensorboard==2.7.0',
-        'pyyaml==6.0',
-        'pytest==6.2.5',
         'atari-py==0.2.6',
+        'coverage==6.3.1',
         'gym==0.18.0',
         'moviepy==1.0.3',
         'opencv-python==4.5.2.52',
+        'torch==1.10.1',
+        'tensorboard==2.7.0',
+        'pytest==6.2.5',
+        'pyyaml==6.0',
     ])
